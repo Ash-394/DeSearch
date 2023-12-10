f12b0b5e06b9a6db5cd7eabdbe4b1065bfa64775
Ash-394 <aswini.at100@gmail.com>
2023-12-10 08:43:50 +0530

    update

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,62 +1,41 @@
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config({ path: ".env" });
 
-require("@nomicfoundation/hardhat-toolbox");
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
 
-// The next line is part of the sample project, you don't need it in your
-// project. It imports a Hardhat task definition, that can be used for
-// testing the frontend.
-;
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
 module.exports = {
-  solidity: "0.8.17",
-};
-
-
-/*
-require("@nomicfoundation/hardhat-toolbox");
-require("@nomicfoundation/hardhat-ethers");
-require("dotenv").config();
-
-/** @type import('hardhat/config').HardhatUserConfig 
-module.exports = {
-  solidity: "0.8.17",
   defaultNetwork: "alfajores",
   networks: {
-      alfajores: {
-          url: "https://alfajores-forno.celo-testnet.org",
-          accounts: [process.env.PRIVATE_KEY],
-          chainId: 44787,
-      },
-      celo: {
-          url: "https://forno.celo.org",
-          accounts: [process.env.PRIVATE_KEY],
-          chainId: 42220,
-    
-      },
+    localhost: {
+      url: "http://127.0.0.1:7545",
+    },
+    alfajores: {
+      url: "https://alfajores-forno.celo-testnet.org",
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 44787,
+    },
+    celo: {
+      url: "https://forno.celo.org",
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 42220,
+    },
   },
-  etherscan: {
-      apiKey: {
-          alfajores: process.env.CELOSCAN_API_KEY,
-          celo: process.env.CELOSCAN_API_KEY,
-      },
-      customChains: [
-          {
-              network: "alfajores",
-              chainId: 44787,
-              urls: {
-                  apiURL: "https://api-alfajores.celoscan.io/api",
-                  browserURL: "https://alfajores.celoscan.io",
-              },
-          },
-          {
-              network: "celo",
-              chainId: 42220,
-              urls: {
-                  apiURL: "https://api.celoscan.io/api",
-                  browserURL: "https://celoscan.io/",
-              },
-          },
-      ],
+  solidity: {
+    version: "0.8.4",
+  },
+  namedAccounts: {
+    deployer: 0,
   },
 };
-*/
